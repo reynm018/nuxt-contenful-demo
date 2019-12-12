efa9c3c7e7a336cbebd0e16360ffd3d5a12d19ae
reynaldo masangkay <reynaldo.m018@gmail.com>
2019-12-12 22:29:28 +0800

    Update gitignore

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['cache-management'] = require('../middleware/cache-management.js');
-middleware['cache-management'] = middleware['cache-management'].default || middleware['cache-management']
-
-export default middleware
